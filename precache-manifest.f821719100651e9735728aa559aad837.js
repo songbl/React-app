709f54c5c25a2e91713487c3a7dddb2ba7ae5d3d
791243928@qq.com <791243928@qq.com>
2019-05-30 20:19:44 +0800

    Updates

---
+++ precache-manifest.f821719100651e9735728aa559aad837.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca5f23c2efd0e0a390e0107e80e1c4b",
+    "url": "/firsrReact/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/firsrReact/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7cc69de8d2ef96839274",
+    "url": "/firsrReact/static/js/main.12d51ad7.chunk.js"
+  },
+  {
+    "revision": "a2095bf0f11bf3a91d89",
+    "url": "/firsrReact/static/js/runtime~main.b28aa0f3.js"
+  }
+]);
